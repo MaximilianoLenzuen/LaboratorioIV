0bb4c050a793cdf9c5fc9cbe95c8fe225e3c9271
unknown <maximilianolenzuen@gmail.com>
2023-09-03 19:26:12 -0300

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'clase1';
+  public edadUno : string = "";
+  public edadDos : string = "";
+
+  public valorSuma : string = "";
+  public valorPromedio : string = "";
+
+  hacerCalculos(): void {
+    this.valorPromedio = ((parseInt(this.edadUno) + parseInt(this.edadDos)) /2).toString();
+    this.valorSuma = (parseInt(this.edadUno) + parseInt(this.edadDos)).toString();
+  }
+
+  limpiarValores(): void {
+    this.edadDos = "";
+    this.edadUno = "";
+    this.valorSuma = "";
+    this.valorPromedio = "";
+  }
+
+}
